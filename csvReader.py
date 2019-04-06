cec07464fb96cbb36aabc3c21b95c0152d72c809
RAlanWright <wright.alan88@gmail.com>
2019-04-05 21:53:17 -0500

    modified for row in readr[1:]:

--- csvReader.py
+++ csvReader.py
@@ -1,7 +1,8 @@
+from datetime import datetime, date
 import csv
 
 # Python with open will auto-close after finished executing
-with open('SystemTasks.csv') as f:
+with open('SystemTaskstest.csv') as f:
     reader = csv.reader(f)
     reader = list(reader) # Need to convert to a list to index / slice it
     # writer = csv.writer(csvfile) # To be used to generate a separate csv file; not currently implemented
@@ -9,15 +10,31 @@ with open('SystemTasks.csv') as f:
     # Create a dict of header name -> column number
     headers = {col.strip(): n for n, col in enumerate(reader[0])}
     # print("headers: ", headers) # Used to display the headers and their index.
-    # Copy these "headers" into headers_i_want below from your first run. Issue copying the emoji from excel into the editor...
+    
 
     # For headers_i_want, copy the output from the above print() for the data you want to display
     headers_i_want = ['Task/Step', 'Start date', 'End date', 'Status']
+    
+    # not_included = ['ing', 'Child', 'Parent', 'Reconcile', 'devices']
+
+    x = 1
     for row in reader[1:]: # Skip the first row because it's the header
-        print(row[0])
-        if (row is not None and row != "Importing" and row != "Export" and row != "Parent" and row != "Gathering " and row != "Uploading"): # Grab students from your section; Change this for your section
-            for h in headers_i_want:
+        if "ing" not in row[0] and "Child" not in row[0] and "Parent" not in row[0] and "Reconcile" not in row[0] and "devices" not in row[0]: 
+        # and "Exporting" not in row[0]"Uploading" not in row[0] and "" :
+            print("=" * 50)
+            print(x, row[0])
+            print("=" * 50)
+            x += 1
+
+         # Print system task, start time, end time, and task status from each task      
+            ## count = 0
+
+            for h in headers_i_want[1:]:
                 print(h, " ", row[headers[h]], end='')
                 print()
-            print("-" * 40) # Separate each student
+            ## # if headers_i_want['End date'] [row[headers[h]] 
+            ## # how_long = datetime.combine(date.min, headers_i_want[2]) - datetime.combine(date.min, headers[1])
+            ## # print("Time: ", how_long)
+            print()
+            print("-" * 50) 
             print()
